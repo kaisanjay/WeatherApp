ba1bcdbfc1b97a9a8e500adf2de1382e5e0a3a50
kaisanjay <sanjaykai.1994@example.com>
2018-04-05 18:30:56 +0530

    setting setState in syn with api

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ const API_KEY = "a6c6a3a831b0736f6f433d06084d72b5";
 
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
@@ -17,6 +25,14 @@ class App extends React.Component {
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`)
     const data = await api_call.json();
     console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
   }
 
 
