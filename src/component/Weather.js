f507e1808b74613895af2bd6363550c678e07c36
kaisanjay <sanjaykai.1994@example.com>
2018-04-05 18:52:21 +0530

    weather component

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -4,10 +4,16 @@ class Weather extends React.Component {
 	render() {
 		return (
 			<div>
-			Weather Component	
+			{this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+			{this.props.temperature && <p>Temperature: {this.props.temperature}</p>}
+			{this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+			
+			{this.props.description && <p>Description: {this.props.description}</p>}
+				
 			</div>
 			);
+			}
 	}
-};
+
 
 export default Weather;
