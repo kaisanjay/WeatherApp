14259b2f4fea541809b518ef2f208a080576e82b
kaisanjay <sanjaykai.1994@example.com>
2018-04-05 18:20:34 +0530

    making api call dynamic

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ const API_KEY = "a6c6a3a831b0736f6f433d06084d72b5";
 class App extends React.Component {
   getWeather = async (e) => {
     e.preventDefault();
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${API_KEY}`)
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`)
     const data = await api_call.json();
     console.log(data);
   }
