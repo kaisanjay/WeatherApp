f13528d3381f4532115bfb9a03d687aa270c2618
kaisanjay <sanjaykai.1994@example.com>
2018-04-06 12:59:34 +0530

    App.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css"
+import './img/bg.jpg'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
